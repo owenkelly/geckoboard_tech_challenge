b6126067d508d7205d3d05165436ca8a609eab26
Owen Kelly <owen.james.kelly@gmail.com>
2016-10-11 19:37:14 -0700

    added get commands for hooking into the twitter API and pushing to geckoboard.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-#to be added
+SETUP: run npm install, and create a .env file in the project's root directory with your
+geckoboard API key in a variable called API_KEY
--- app.js
+++ app.js
@@ -1,6 +1,42 @@
 require('dotenv').config();
-var API_KEY = process.env.API_KEY;
-var gb = require('geckoboard')(API_KEY);
+var gb = require('geckoboard')(process.env.API_KEY);
+var Twitter = require('twitter');
+var lastTweetId = 1;
+var lastRetweetId = 1;
+var client = new Twitter({
+  consumer_key: process.env.TW_API_KEY,
+  consumer_secret: process.env.TW_API_SECRET,
+  access_token_key: process.env.TW_ACCESS_TOKEN,
+  access_token_secret: process.env.TW_ACCESS_TOKEN_SECRET
+});
+
+var tweetCount = client.get('statuses/user_timeline', {since_id: lastTweetId, count: 100}, function(error, tweets, response){
+  if(error) {
+    console.log(error);
+    return;
+  }
+
+  return tweets.length;
+});
+
+var retweetCount = client.get('statuses/retweets_of_me', {since_id: lastRetweetId}, function(error, retweets, response){
+  if(error){
+    console.log(error);
+    return;
+  }
+
+  return retweets.length;
+});
+
+var followersCount = client.get('followers/ids', function(error, ids, response){
+  if(error){
+    console.log(error);
+    return;
+  }
+  
+  return ids.ids.length;
+});
+
 
 gb.ping(function (error){
   if (error) {
@@ -10,3 +46,48 @@ gb.ping(function (error){
 
   console.log("Authentication Sucessful");
 });
+
+gb.datasets.findOrCreate(
+  {
+    id: 'twitter_account.by_hour',
+    fields: {
+      tweets: {
+        type: 'number',
+        name: 'Number of tweets'
+      },
+      retweets: {
+        type: 'number',
+        name: 'Number of retweets'
+      },
+      followers: {
+        type: 'number',
+        name: 'Number of twitter followers'
+      },
+      datetime: {
+        type: 'datetime',
+        name: 'Datetime'
+      }
+    }
+  },
+  function (error, dataset){
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+    dataset.put(
+      [
+      {datetime: new Date().toISOString(), tweets: tweetCount, retweets: retweetCount,
+      followers: followersCount}
+      ],
+      function(error){
+        if (error){
+          console.log(error);
+          return
+        }
+        console.log('Dataset created and data added');
+      }
+    );
+  }
+);
+
